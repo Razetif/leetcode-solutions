9c4a038f8c7fdf0abba1922f8d8511501dc3e2bf
Razetif <salehzaidan64@gmail.com>
2025-02-23 16:14:59 +0700

    merge-strings-alternately: solve with one pointer

--- problems/merge-strings-alternately.cpp
+++ problems/merge-strings-alternately.cpp
@@ -84,3 +84,40 @@ TEST_F(MergeAlternatelyTest2, EdgeCases) {
     EXPECT_EQ(solution.mergeAlternately("a", "bcd"), "abcd");
     EXPECT_EQ(solution.mergeAlternately("a", "b"), "ab");
 }
+
+class Solution3 {
+  public:
+    string mergeAlternately(string word1, string word2) {
+        string result;
+        size_t maxLength = max(word1.length(), word2.length());
+
+        for (size_t i = 0; i < maxLength; i++) {
+            if (i < word1.length()) {
+                result += word1[i];
+            }
+
+            if (i < word2.length()) {
+                result += word2[i];
+            }
+        }
+
+        return result;
+    }
+};
+
+class MergeAlternatelyTest3 : public ::testing::Test {
+  protected:
+    Solution3 solution;
+};
+
+TEST_F(MergeAlternatelyTest3, BasicCases) {
+    EXPECT_EQ(solution.mergeAlternately("abc", "pqr"), "apbqcr");
+    EXPECT_EQ(solution.mergeAlternately("ab", "pqrs"), "apbqrs");
+    EXPECT_EQ(solution.mergeAlternately("abcd", "pq"), "apbqcd");
+}
+
+TEST_F(MergeAlternatelyTest3, EdgeCases) {
+    EXPECT_EQ(solution.mergeAlternately("abc", "d"), "adbc");
+    EXPECT_EQ(solution.mergeAlternately("a", "bcd"), "abcd");
+    EXPECT_EQ(solution.mergeAlternately("a", "b"), "ab");
+}
